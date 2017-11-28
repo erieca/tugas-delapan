bc64d6b74a1c3a185f37aa056d358e8797e74456
erieca <33479931+erieca@users.noreply.github.com>
2017-11-28 10:36:32 +0700

    Update no 1.cpp

--- no 1.cpp
+++ no 1.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
 using namespace std;
-
-int max(int nomer[])
+int main()
 {
-	int terbesar;
-	
-	for(int i=0; i<10; i++){
-		if(i == 0 || nomer[i] > terbesar)
-			terbesar=nomer[i];
-		}
-		return terbesar;
-	}
-int min(int nomer[]){
-	
-	int terkecil;
-	
-	for(int i=0;i<10;i++){
-		if(i==0 || nomer[i] < terkecil)
-			terkecil=nomer[i];
-	}
-	return terkecil;
-}
-int main(){
-	
-	int nomer[10];
-	
-	for(int i=0;i<10;i++){
-		cout<<"Masukkan angka ke-"<<i+1<<":";
-		cin>>nomer[i];
-	}
-	cout<<"Angka terbesar: "<<max(nomer)<<endl;
-	cout<<"Angka terkecil: "<<min(nomer)<<endl;
+    int nilai[10],i,min,maks;
+    
+    for(i=0;i<=9;i++){
+        cout<<"Masukkan nilai ke-"<<i+1<<":";
+        cin>>nilai[i];
+    }
+    
+   min = nilai[0];
+   maks = nilai[0];
+   for(i=0;i<=9;i++){
+        if(nilai[i] < min){
+            min = nilai[i];
+   }
+        else if(nilai[i] > maks){
+            maks = nilai[i];
+   }
+   }
+    cout<<"Nilai minimum adalah : "<<min<<endl;
+    cout<<"Nilai maksimum adalah : "<<maks<<endl;
+   
+    return 0;
 }
